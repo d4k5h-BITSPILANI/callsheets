778d512bb39051ebbb3dc37ea384c34b109094c9
Daksh Kriplani <f20212822@goa.bits-pilani.ac.in>
2024-07-18 13:14:59 +0530

    major changes

--- src/OTP.jsx
+++ src/OTP.jsx
@@ -6,45 +6,52 @@
 
   const OTP = () => {
     const [isButtonDisabled, setIsButtonDisabled] = useState(false);
-    const [otp, setOtp] = useState('');
+    const [otp, setOtp] = useState(Array(6).fill('')); // Initialize with empty strings for each digit;
     const { login } = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
     const { phoneNumber,id,name,email,project_id,callsheet_id } = location.state || {};
 
-    
-    const verifyOTP = async () => {
-      if (otp.length !== 6) {
-        alert("Please enter a valid 6-digit OTP.");
-        return;
-      }
-      setIsButtonDisabled(true);
-
-      const payload = {
-        is_accepted: "true",
-        isd_code: "+91",
-        otp: otp,
-        username: phoneNumber,
-      };
-
-      try {
-        await axios.post('https://prod-fbm-auth-service.ashymeadow-b2171eb1.centralindia.azurecontainerapps.io/auth-service/authentication', payload, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Origin': 'https://myfilmapp.com',
-            'Referer': 'https://myfilmapp.com/',
-          }
-        });
-        login();  
-        navigate('/upload',{ state: { phoneNumber,id,name,project_id,callsheet_id,email } });  
-      } catch (error) {
-
-          alert('Error verifying OTP');
-          setIsButtonDisabled(false);
-          return;
-        
+    const handleChange = (value, index) => {
+      const newOtp = [...otp];
+      newOtp[index] = value;
+      setOtp(newOtp);
+      if (value.length === 1 && index < 5) {
+        // Automatically focus next input
+        document.getElementById(`otp-${index + 1}`).focus();
       }
     };
+
+  const verifyOTP = async () => {
+    const fullOTP = otp.join('');
+    if (fullOTP.length !== 6) {
+      alert("Please enter a valid 6-digit OTP.");
+      return;
+    }
+    setIsButtonDisabled(true);
+    const payload = {
+      is_accepted: "true",
+      isd_code: "+91",
+      otp: fullOTP,
+      username: phoneNumber,
+    };
+    try {
+      await axios.post('https://prod-fbm-auth-service.ashymeadow-b2171eb1.centralindia.azurecontainerapps.io/auth-service/authentication', payload, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Origin': 'https://myfilmapp.com',
+          'Referer': 'https://myfilmapp.com/',
+        }
+      });
+      login();  
+      navigate('/upload', { state: { phoneNumber, id, name, project_id, callsheet_id, email } });
+    } catch (error) {
+      alert('Error verifying OTP');
+      setIsButtonDisabled(false);
+    }
+  };
+
+
     useEffect(() => {
       const handleKeyPress = (event) => {
           if (event.key === 'Enter' && phoneNumber.length === 10) {
@@ -60,6 +67,14 @@
           document.removeEventListener('keydown', handleKeyPress);
       };
   }, [otp]); 
+
+  useEffect(() => {
+    // Verify OTP automatically when all digits are filled
+    const fullOTP = otp.join('');
+    if (fullOTP.length === 6) {
+      verifyOTP();
+    }
+  }, [otp]);
 
   useEffect(() => {
     const handleNavigate = () => {
@@ -79,26 +94,31 @@
     return (
       <div className="container">
         <div className="left-container">
-        <img src='https://bbzeapqhbmyrggqphapv.supabase.co/storage/v1/object/public/callsheets/WhatsApp_Image_2024-06-10_at_00.15.48_fca40442.jpg' alt="Descriptive Alt Text" style={{ width: '100%', height: '100%' }} />
+        <img width='300px' src='https://bbzeapqhbmyrggqphapv.supabase.co/storage/v1/object/public/callsheets/WhatsApp_Image_2024-06-10_at_00.15.48_fca40442.jpg' alt="Descriptive Alt Text" style={{ width: '100%', height: '100%' }} />
         </div>
         <div className="right1-container">
           <h1 className='heading'>Verification Code</h1>
-          <p className='paragraph2'>We have sent the verification</p>
-          <p className='paragraph2'>code to your mobile number</p>
+          <p className='paragraph2'>We have sent the verification code to your</p>
+          <p className='paragraph2'> mobile number</p>
           <p className='paragraph3'>+91 {phoneNumber}</p>
           
           <div className='model'>
-            <div className='phone-input'>
-            <input
-            className='input1'
-            type="text"
-            value={otp}
-            onChange={(e) => setOtp(e.target.value)}
-            placeholder="OTP"
-          />
-            </div>
+          <div className='phone-input'>
+            {otp.map((digit, index) => (
+              <input
+                key={index}
+                id={`otp-${index}`}
+                className='input1'
+                type="text"
+                value={digit}
+                maxLength="1"
+                onChange={e => handleChange(e.target.value, index)}
+                autoFocus={index === 0}
+              />
+            ))}
+          </div>
           <button className='henry' onClick={verifyOTP} disabled={isButtonDisabled}
-          style={{ backgroundColor: isButtonDisabled ? 'grey' : '#FC4E00' }}>Continue</button>
+          style={{ backgroundColor: isButtonDisabled ? 'grey' : '#FC4E00' }}>Login</button>
           <div className='code-receiver'>
           <p className='paragraph2'>Didn't receive code?</p>
           <p className='please' onClick={() =>{
